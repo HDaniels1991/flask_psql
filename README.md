802356ed4c41b0d8796e99cd53b95b53c2ff0e65
Harry Daniels <harry.daniels@annalect.com>
2019-04-21 13:20:31 +0100

    created flask - psql app

---
+++ README.md
@@ -0,0 +1,11 @@
+# Making a Flask app using PostgreSQL
+
+This is a simple app to demonstrate how you might create a Flask application linked to a PostgreSQL database.
+
+## Requirements
+
+## Base Flask app
+
+## Author
+
+Harry Daniels
---
+++ app.py
@@ -0,0 +1,84 @@
+import os
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import (StringField,SubmitField,IntegerField)
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+# Connects our Flask App to our Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/my_website'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+################
+#####MODELS#####
+################
+
+# Create our database model
+class Person(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    first = db.Column(db.Text)
+    last = db.Column(db.Text)
+
+    def __init__(self, first,last):
+        self.first = first
+        self.last = last
+
+    def __repr__(self):
+        return f"Person {self.id}: {self.first} {self.last}"
+
+################
+#####FORMS######
+################
+
+class AddForm(FlaskForm):
+    first = StringField('First Name',validators=[DataRequired()])
+    last = StringField('Last Name',validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class DelForm(FlaskForm):
+    id = IntegerField('Id Person to Remove:')
+    submit = SubmitField('Remove Person')
+
+################
+#####VIEWS######
+################
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add',methods=['GET','POST'])
+def add():
+    form = AddForm()
+    if form.validate_on_submit():
+        first = form.first.data
+        last = form.last.data
+        new_person = Person(first=first,last=last)
+        db.session.add(new_person)
+        db.session.commit()
+        return redirect(url_for('list_people'))
+    return render_template('add.html',form=form)
+
+@app.route('/del',methods=['GET','POST'])
+def delete():
+    form = DelForm()
+    if form.validate_on_submit():
+        id = form.id.data
+        person = Person.query.get(id)
+        if person:
+            db.session.delete(person)
+            db.session.commit()
+        return redirect(url_for('list_people'))
+    return render_template('del.html',form=form)
+
+@app.route('/list', methods=['GET','POST'])
+def list_people():
+    people = Person.query.all()
+    return render_template('list.html', people=people)
+
+if __name__ == '__main__':
+    app.run(debug=True)
